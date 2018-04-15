1f19648d1c97f21e23b177742a35ec0a0b439eab
Deep Patel <deeppatel234@gmail.com>
2018-04-15 21:30:52 +0530

    Fix DB connectios

--- db.js
+++ db.js
@@ -10,47 +10,62 @@ var MongoClient = require('mongodb').MongoClient
 // Internal Functions
 var logger = require('./lib/base/logger').getLogger()
 
-// Init DB State
-var state = { db: null }
-
-/**
- * For Connect to MongoDB Instance
- *
- * @param {object} connection
- * @param {callback} cb
- */
-exports.connect = function (connection, cb) {
-  if (state.db) return cb()
-
-  MongoClient.connect(connection.url, function (err, client) {
-    if (err) return cb(err)
-    state.db = client.db(connection.name)
-    logger.info('MongoDB Connected at :', connection.url, 'Database:', connection.name)
-    cb()
-  })
-}
-
-/**
- * get mongodb cursor for db operations
- *
- */
-exports.get = function () {
-  return state.db
-}
-
-/**
- * close mongodb connection
- *
- * @param {callback} cb
- */
-exports.close = function (cb) {
-  if (state.db) {
-    state.db.close(function (err, result) {
-      if (err) {
-        logger.info('MongoDB Closing Connection Error :', err)
-        cb(err)
-      }
-      state.db = null
+class Connection {
+  constructor (props) {
+    this.db = null
+    this.client = null
+  }
+  /**
+   * For Connect to MongoDB Instance
+   *
+   * @param {object} connection
+   * @param {object} options
+   * @memberof Connection
+   */
+  connect (connection, options = {}) {
+    if (this.db) {
+      return new Promise((resolve) => resolve())
+    }
+    var self = this
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(connection.url, options).then(function (client) {
+        self.client = client
+        self.db = client.db(connection.name)
+        logger.info('MongoDB Connected at :', connection.url, 'Database:', connection.name)
+        resolve(client)
+      }).catch(function (err) {
+        logger.error(err)
+        reject(err)
+      })
     })
   }
+  /**
+   * get mongodb cursor for db operations
+   * @returns connection
+   * @memberof Connection
+   */
+  get () {
+    return this.db
+  }
+  /**
+   * close mongodb connection
+   * @param {boolean} force
+   * @memberof Connection
+   */
+  close (force = false) {
+    if (this.db) {
+      var self = this
+      return new Promise((resolve, reject) => {
+        self.client.close(force).then(function () {
+          logger.info('MongoDB Closing Connection')
+          self.db = null
+          self.client = null
+        }).catch(function (err) {
+          logger.info('MongoDB Closing Connection Error :', err)
+        })
+      })
+    }
+  }
 }
+
+module.exports = new Connection()
