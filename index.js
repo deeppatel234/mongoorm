0c98928c3fc0da3bf94aab421e9f3f9118bd86d8
Deep Patel <deeppatel234@gmail.com>
2018-07-29 16:39:12 +0530

    exports object id

--- index.js
+++ index.js
@@ -5,11 +5,15 @@
  */
 const logger = require('./lib/base/Logger');
 const connection = require('./Connection');
+const mongodb = require('mongodb');
+const mongoutils = require('./lib/base/MongoUtils');
 
 exports.connect = (uri, options) => connection.connect(uri, options);
 exports.close = force => connection.close(force);
 
 exports.setLogger = log => logger.setLogger(log);
+exports.ObjectId = mongodb.ObjectID;
+exports.getObjectID = mongoutils.getObjectID;
 
 exports.Fields = require('./lib/fields');
 exports.Document = require('./lib/document/Document');
